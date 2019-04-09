23333b8c0fa00b9e2f24fcf314f56d9aab91099c
Comal Virdi <comalvirdi@Comals-MacBook-Pro-10.local>
2019-04-09 10:15:12 -0700

    hopefully last commit

--- lab1.py
+++ lab1.py
@@ -6,11 +6,10 @@
 from math import *
 
 
-
-def max_list_iter(int_list):  
-# must use iteration not recursion
-	"""finds the max of a list of numbers and returns the value (not the index)
-	If int_list is empty, returns None. If list is None, raises ValueError"""
+# this function finds the max of a list of numbers and returns the value (not the index)
+#If int_list is empty, returns None. If list is None, raises ValueError 
+# list --> int
+def max_list_iter(int_list):  	
 	if int_list == None:
 		raise ValueError
 	if len(int_list) == 0:
@@ -25,10 +24,11 @@ def max_list_iter(int_list):
 
 
 
-
-def reverse_rec(int_list):   # must use recursion
-	"""recursively reverses a list of numbers and returns the reversed list
+"""recursively reverses a list of numbers and returns the reversed list
 	If list is None, raises ValueError"""
+# list --> list
+def reverse_rec(int_list):   # must use recursion
+	
 	if int_list == None:
 		raise ValueError
 	if len(int_list)==1:
@@ -36,9 +36,14 @@ def reverse_rec(int_list):   # must use recursion
 	return reverse_rec(int_list[1:]) + [int_list[0]]
 	
 
-def bin_search(target, low, high, int_list):  # must use recursion
-	"""searches for target in int_list[low..high] and returns index if found
+
+"""searches for target in int_list[low..high] and returns index if found
 	If target is not found returns None. If list is None, raises ValueError """
+	# int int int list --> int
+def bin_search(target, low, high, int_list):  
+	
+	if int_list == None:
+		raise ValueError
 	if high < low:
 		return None
 	mid = (high+low)//2
--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -11,40 +11,51 @@ class TestLab1(unittest.TestCase):
             max_list_iter(tlist)
 
     def test_max_list_iter_maxes(self):
-        self.assertEqual(max_list_iter([1,2,3,4,5]), 5)
-        self.assertEqual(max_list_iter([2,2,1,1,1]), 2)
-        self.assertEqual(max_list_iter([5,1,5,1]), 5)
-        self.assertEqual(max_list_iter([5,5,5]), 5)
-        self.assertEqual(max_list_iter([1,5,1]), 5)
-        self.assertEqual(max_list_iter([5,1,5]), 5)
-        self.assertEqual(max_list_iter([5,5,1]), 5)
+        self.assertEqual(max_list_iter([1,2,3,4,5]), 5) # tests basic max function
+        self.assertEqual(max_list_iter([2,2,1,1,1]), 2) # tests with multiple same min and max vals
+        self.assertEqual(max_list_iter([1,2,5,1,2]), 5) # tests with max in middle
+        self.assertEqual(max_list_iter([5,5,5]), 5) # tests when list elements are all one number
+        self.assertEqual(max_list_iter([1,5,1]), 5) # max is in the middle
+        self.assertEqual(max_list_iter([5,1,1]), 5) # max at beginnging
+        self.assertEqual(max_list_iter([1,2,5]), 5) # Max at end
         
 
 
+    def test_reverse_rec_val_error(self):
+        tlist = None
+        with self.assertRaises(ValueError):  # used to check for exception
+            reverse_rec(tlist)
+
     def test_reverse_rec(self):
-        self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
-        self.assertEqual(reverse_rec([1,2,3,4,5,6,7,8]),[8,7,6,5,4,3,2,1])
-        self.assertEqual(reverse_rec([1,2,3,2,1]),[1,2,3,2,1])
-        self.assertEqual(reverse_rec([1,2]),[2,1])
+        self.assertEqual(reverse_rec([1,2,3]),[3,2,1]) # simple reversal
+        self.assertEqual(reverse_rec([1,2,3,4,5,6,7,8]),[8,7,6,5,4,3,2,1]) # longer reversal
+        self.assertEqual(reverse_rec([1,2,3,2,1]),[1,2,3,2,1]) # reversal is same as original
+        self.assertEqual(reverse_rec([1,2]),[2,1]) # simple reversal with two numbers
 
+    
 
-
+    def test_bin_search_val_error(self):
+        tlist = None
+        with self.assertRaises(ValueError):  # used to check for exception
+            bin_search(1, 2, 3,tlist)
 
     def test_bin_search(self):
         list_val =[0,1,2,3,4,7,8,9,10]
         low = 0
         high = len(list_val)-1
-        self.assertEqual(bin_search(4, low, high, list_val), 4 )
-        self.assertEqual(bin_search(0, low, high, list_val), 0 )
-        self.assertEqual(bin_search(1, low, high, list_val), 1 )
-        self.assertEqual(bin_search(2, low, high, list_val), 2 )
-        self.assertEqual(bin_search(3, low, high, list_val), 3 )
-        self.assertEqual(bin_search(5, low, high, list_val), None )
-        self.assertEqual(bin_search(7, low, high, list_val), 5 )
-        self.assertEqual(bin_search(8, low, high, list_val), 6 )
-        self.assertEqual(bin_search(9, low, high, list_val), 7 )
-        self.assertEqual(bin_search(10, low, high, list_val), 8 )
-        self.assertEqual(bin_search(11, low, high, list_val), None )
+        
+        self.assertEqual(bin_search(0, low, high, list_val), 0 ) # tests target at beginning
+        self.assertEqual(bin_search(1, low, high, list_val), 1 ) # tests target in pos 1
+        self.assertEqual(bin_search(2, low, high, list_val), 2 ) # tests target in pos 2
+        self.assertEqual(bin_search(3, low, high, list_val), 3 ) # tests target in pos 3
+        self.assertEqual(bin_search(4, low, high, list_val), 4 ) # tests target in middle
+        self.assertEqual(bin_search(7, low, high, list_val), 5 ) # tests target in pos 5
+        self.assertEqual(bin_search(8, low, high, list_val), 6 ) # tests target in pos 6
+        self.assertEqual(bin_search(9, low, high, list_val), 7 ) # tests target in pos 7
+        self.assertEqual(bin_search(10, low, high, list_val), 8 ) # tests target at the end
+
+        self.assertEqual(bin_search(11, low, high, list_val), None ) # tests target not in list
+        self.assertEqual(bin_search(5, low, high, list_val), None ) # tests target not in list
 
 
 
