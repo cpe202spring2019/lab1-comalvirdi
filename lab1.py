c54ef50f7db5b0d0178113e6fb97002521a9fa18
Comal Virdi <comalvirdi@Comals-MacBook-Pro-10.local>
2019-04-09 09:44:36 -0700

    Draft

--- lab1.py
+++ lab1.py
@@ -1,15 +1,53 @@
+# Comal Virdi
+# CPE 202
 
-def max_list_iter(int_list):  # must use iteration not recursion
-   """finds the max of a list of numbers and returns the value (not the index)
-   If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+
+
+from math import *
+
+
+
+def max_list_iter(int_list):  
+# must use iteration not recursion
+	"""finds the max of a list of numbers and returns the value (not the index)
+	If int_list is empty, returns None. If list is None, raises ValueError"""
+	if int_list == None:
+		raise ValueError
+	if len(int_list) == 0:
+		return None
+	max_temp = float('-inf')
+	for x in int_list:
+		if x > max_temp:
+			max_temp = x
+		elif x == max_temp:
+			max_temp = x
+	return max_temp
+
+
+
 
 def reverse_rec(int_list):   # must use recursion
-   """recursively reverses a list of numbers and returns the reversed list
-   If list is None, raises ValueError"""
-   pass
+	"""recursively reverses a list of numbers and returns the reversed list
+	If list is None, raises ValueError"""
+	if int_list == None:
+		raise ValueError
+	if len(int_list)==1:
+		return int_list
+	return reverse_rec(int_list[1:]) + [int_list[0]]
+	
 
 def bin_search(target, low, high, int_list):  # must use recursion
-   """searches for target in int_list[low..high] and returns index if found
-   If target is not found returns None. If list is None, raises ValueError """
-   pass
+	"""searches for target in int_list[low..high] and returns index if found
+	If target is not found returns None. If list is None, raises ValueError """
+	if high < low:
+		return None
+	mid = (high+low)//2
+	if int_list[mid] == target:
+		return mid
+	if int_list[mid] > target:
+		high = mid - 1
+	if int_list[mid] < target:
+		low = mid + 1 
+	return bin_search(target, low, high, int_list)
+
+
--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -1,23 +1,51 @@
-aimport unittest
+import unittest
 from lab1 import *
 
  # A few test cases.  Add more!!!
 class TestLab1(unittest.TestCase):
 
-    def test_max_list_iter(self):
+    def test_max_list_iter_val_error(self):
         """add description here"""
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
 
+    def test_max_list_iter_maxes(self):
+        self.assertEqual(max_list_iter([1,2,3,4,5]), 5)
+        self.assertEqual(max_list_iter([2,2,1,1,1]), 2)
+        self.assertEqual(max_list_iter([5,1,5,1]), 5)
+        self.assertEqual(max_list_iter([5,5,5]), 5)
+        self.assertEqual(max_list_iter([1,5,1]), 5)
+        self.assertEqual(max_list_iter([5,1,5]), 5)
+        self.assertEqual(max_list_iter([5,5,1]), 5)
+        
+
+
     def test_reverse_rec(self):
         self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
+        self.assertEqual(reverse_rec([1,2,3,4,5,6,7,8]),[8,7,6,5,4,3,2,1])
+        self.assertEqual(reverse_rec([1,2,3,2,1]),[1,2,3,2,1])
+        self.assertEqual(reverse_rec([1,2]),[2,1])
+
+
+
 
     def test_bin_search(self):
         list_val =[0,1,2,3,4,7,8,9,10]
         low = 0
         high = len(list_val)-1
-        self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
+        self.assertEqual(bin_search(4, low, high, list_val), 4 )
+        self.assertEqual(bin_search(0, low, high, list_val), 0 )
+        self.assertEqual(bin_search(1, low, high, list_val), 1 )
+        self.assertEqual(bin_search(2, low, high, list_val), 2 )
+        self.assertEqual(bin_search(3, low, high, list_val), 3 )
+        self.assertEqual(bin_search(5, low, high, list_val), None )
+        self.assertEqual(bin_search(7, low, high, list_val), 5 )
+        self.assertEqual(bin_search(8, low, high, list_val), 6 )
+        self.assertEqual(bin_search(9, low, high, list_val), 7 )
+        self.assertEqual(bin_search(10, low, high, list_val), 8 )
+
+
 
 if __name__ == "__main__":
         unittest.main()
